a35edddc181245db6732efe38ef2db7c33e216d5
Zerio <neoboij@gmail.com>
2023-12-04 18:40:30 +0100

    feat: setup `get_package` function

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,42 @@ pub fn get_public_api_key() -> Result<String, String> {
     }
 }
 
+pub async fn get_package(package_id: i32) -> Result<Package, String> {
+    let api_key = get_public_api_key();
+
+    match api_key {
+        Ok (api_key) => {
+            let res = reqwest::get(format!("{0}/accounts/{1}/packages/{2}", BASE_URL, api_key, package_id))
+                .await;
+
+            match res {
+                Ok (data) => {
+                    let json = data.json::<Data<Package>>()
+                        .await;
+
+                    match json {
+                        Ok (json_data) => {
+                            return Ok(json_data.data);
+                        }
+
+                        Err(err) => {
+                            return Err(String::from(format!("Error occured whilst parsing JSON, {0}", err)))
+                        }
+                    }
+                }
+
+                _ => {
+                    return Err(String::from("Unknown error occured"));
+                }
+            }
+        }
+
+        _ => {
+            return Err(String::from("Unknown error occured"));
+        }
+    }
+}
+
 pub async fn get_all_packages() -> Result<Vec<Package>, String> {
     let api_key = get_public_api_key();
 
@@ -85,5 +121,18 @@ mod tests {
     #[tokio::test]
     async fn try_fetch_packages() {
         get_all_packages().await.expect("Packages could not be fetched.");
+    }
+
+    #[tokio::test]
+    async fn try_fetch_package() -> Result<(), String> {
+        let packages = get_all_packages().await.expect("Packages could not be fetched.");
+
+        if packages.len() < 1 {
+            return Err(String::from("Cant test this, the length of packages has to atleast be 1"));
+        }
+
+        get_package(packages[0].id).await.expect("Package could not be fetched");
+
+        return Ok(())
     }
 }
