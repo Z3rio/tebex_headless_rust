0cb9c9e0df5a51bbef4a979030ab965844d56adf
Zerio <neoboij@gmail.com>
2023-12-04 19:47:18 +0100

    chore: better structure

---
+++ src/handlers/package.rs
@@ -0,0 +1,73 @@
+use crate::{structs::{Package, Data}, get_public_api_key, BASE_URL};
+
+pub async fn get_package(package_id: i32) -> Result<Package, String> {
+    let api_key = get_public_api_key();
+
+    match api_key {
+        Ok (api_key) => {
+            let res = reqwest::get(format!("{0}/accounts/{1}/packages/{2}", BASE_URL, api_key, package_id))
+                .await;
+
+            match res {
+                Ok (data) => {
+                    let json = data.json::<Data<Package>>()
+                        .await;
+
+                    match json {
+                        Ok (json_data) => {
+                            return Ok(json_data.data);
+                        }
+
+                        Err(err) => {
+                            return Err(String::from(format!("Error occured whilst parsing JSON, {0}", err)))
+                        }
+                    }
+                }
+
+                _ => {
+                    return Err(String::from("Unknown error occured"));
+                }
+            }
+        }
+
+        _ => {
+            return Err(String::from("Unknown error occured"));
+        }
+    }
+}
+
+pub async fn get_all_packages() -> Result<Vec<Package>, String> {
+    let api_key = get_public_api_key();
+
+    match api_key {
+        Ok (api_key) => {
+            let res = reqwest::get(format!("{0}/accounts/{1}/packages", BASE_URL, api_key))
+                .await;
+
+            match res {
+                Ok (data) => {
+                    let json = data.json::<Data<Vec<Package>>>()
+                        .await;
+
+                    match json {
+                        Ok (json_data) => {
+                            return Ok(json_data.data);
+                        }
+
+                        Err(err) => {
+                            return Err(String::from(format!("Error occured whilst parsing JSON, {0}", err)))
+                        }
+                    }
+                }
+
+                _ => {
+                    return Err(String::from("Unknown error occured"));
+                }
+            }
+        }
+
+        _ => {
+            return Err(String::from("Unknown error occured"));
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-mod structs;
-
-use crate::structs::{Package, Data};
+pub mod structs;
+pub mod handlers;
 
 use std::env::VarError;
 use dotenv::dotenv;
 
-static BASE_URL: &str = "https://headless.tebex.io/api";
+pub static BASE_URL: &str = "https://headless.tebex.io/api";
 
 pub fn set_public_api_key(api_key: String) {
     std::env::set_var("PUBLIC_API_KEY", api_key);
@@ -21,78 +20,6 @@ pub fn get_public_api_key() -> Result<String, String> {
         
         _ => {
             return Err(String::from("Public API key is not set"));
-        }
-    }
-}
-
-pub async fn get_package(package_id: i32) -> Result<Package, String> {
-    let api_key = get_public_api_key();
-
-    match api_key {
-        Ok (api_key) => {
-            let res = reqwest::get(format!("{0}/accounts/{1}/packages/{2}", BASE_URL, api_key, package_id))
-                .await;
-
-            match res {
-                Ok (data) => {
-                    let json = data.json::<Data<Package>>()
-                        .await;
-
-                    match json {
-                        Ok (json_data) => {
-                            return Ok(json_data.data);
-                        }
-
-                        Err(err) => {
-                            return Err(String::from(format!("Error occured whilst parsing JSON, {0}", err)))
-                        }
-                    }
-                }
-
-                _ => {
-                    return Err(String::from("Unknown error occured"));
-                }
-            }
-        }
-
-        _ => {
-            return Err(String::from("Unknown error occured"));
-        }
-    }
-}
-
-pub async fn get_all_packages() -> Result<Vec<Package>, String> {
-    let api_key = get_public_api_key();
-
-    match api_key {
-        Ok (api_key) => {
-            let res = reqwest::get(format!("{0}/accounts/{1}/packages", BASE_URL, api_key))
-                .await;
-
-            match res {
-                Ok (data) => {
-                    let json = data.json::<Data<Vec<Package>>>()
-                        .await;
-
-                    match json {
-                        Ok (json_data) => {
-                            return Ok(json_data.data);
-                        }
-
-                        Err(err) => {
-                            return Err(String::from(format!("Error occured whilst parsing JSON, {0}", err)))
-                        }
-                    }
-                }
-
-                _ => {
-                    return Err(String::from("Unknown error occured"));
-                }
-            }
-        }
-
-        _ => {
-            return Err(String::from("Unknown error occured"));
         }
     }
 }
@@ -120,18 +47,18 @@ mod tests {
 
     #[tokio::test]
     async fn try_fetch_packages() {
-        get_all_packages().await.expect("Packages could not be fetched.");
+        handlers::package::get_all_packages().await.expect("Packages could not be fetched.");
     }
 
     #[tokio::test]
     async fn try_fetch_package() -> Result<(), String> {
-        let packages = get_all_packages().await.expect("Packages could not be fetched.");
+        let packages = handlers::package::get_all_packages().await.expect("Packages could not be fetched.");
 
         if packages.len() < 1 {
             return Err(String::from("Cant test this, the length of packages has to atleast be 1"));
         }
 
-        get_package(packages[0].id).await.expect("Package could not be fetched");
+        handlers::package::get_package(packages[0].id).await.expect("Package could not be fetched");
 
         return Ok(())
     }
