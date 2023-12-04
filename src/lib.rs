f1edfb160b1b2cfcfa62fe3ad1e46d1251139a33
Zerio <neoboij@gmail.com>
2023-12-04 13:38:25 +0000

    feat: setup env vars + simple testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod structs;
+
+use std::env::VarError;
+
+use dotenv::dotenv;
+
+pub fn get_env_var(query: &str) -> Result<String, VarError> {
+    if !dotenv().is_ok() {
+        dotenv().ok();
+    }
+
+    let queried: Result<String, VarError> = std::env::var(query);
+
+    return queried;
 }
 
 #[cfg(test)]
@@ -7,8 +19,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn check_env_vars() {
+        let public_key = get_env_var("TEBEX_PUBLIC_KEY");
+
+        public_key.expect("TEBEX_PUBLIC_KEY is not present in your .env file");
     }
 }
