892dbca4fbdc5b616cc0a59546c7ef965feb02f3
Zerio <neoboij@gmail.com>
2023-12-05 19:34:39 +0100

    fix: incorrect `Message` structure

--- src/models/misc.rs
+++ src/models/misc.rs
@@ -6,6 +6,23 @@ pub struct Data<T> {
 }
 
 #[derive(Deserialize, Serialize, Clone)]
+pub struct FieldOfDetail {
+    
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct Meta {
+    
+}
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Message {
-    pub message: String
+    pub status: Option<i32>,
+    pub r#type: Option<String>,
+    pub title: Option<String>,
+    pub detail: Option<String>,
+    pub error_code: Option<String>,
+    pub field_of_details: Option<Vec<FieldOfDetail>>,
+    pub meta: Option<Vec<Meta>>,
+    pub message: Option<String>
 }
